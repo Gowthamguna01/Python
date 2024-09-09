3d632a7c5f50ae5201a0ac6c884b9cc472e4d715
Gowtham <165543758+Gowthamguna01@users.noreply.github.com>
2024-09-09 07:31:17 +0530

    Set Hands-on

---
+++ set/set Ex1.py
@@ -0,0 +1,5 @@
+sampleset={"yellow","orange","black"}
+samplelist=["blue","green","red"]
+sampleset.update(["blue","green","red"])
+print(sampleset)
+
---
+++ set/set Ex7.py
@@ -0,0 +1,4 @@
+set1={10,20,30,40,50}
+set2={60,70,80,90,10}
+z=set1.intersection(set2)
+print(z)
