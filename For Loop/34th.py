5c3b7b0445299f15024c0a77a93b71507c70b8e0
Gowtham <165543758+Gowthamguna01@users.noreply.github.com>
2024-09-21 20:53:40 -0700

    Update 34th.py

--- For Loop/34th.py
+++ For Loop/34th.py
@@ -1,7 +1,13 @@
 n=int(input("enter n number="))
-for i in range(n):
-    for j in range(n-i-1):
-        print(" ",end="")
-    for j in range(i+1):
-        print("*",end=" ")
-    print()
+s=0
+for i in range(1,n,1):
+    if(n%i==0):
+        s=s+i
+
+
+if(s==n):
+    print("Perfect Number")
+
+else:
+    print("Not a Perfect Number")
+    
